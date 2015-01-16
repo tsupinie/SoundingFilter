08035cd55fd1779802943e30bbbc5e07d372e291
Greg Blumberg <wblumberg@ou.edu>
2015-01-16 14:52:18 -0600

    added tropopause finding code

--- sndfilter.py
+++ sndfilter.py
@@ -14,7 +14,7 @@ def _findIsothermals(temp, pres, tol=0.5):
     cand_layers = np.where(lr_layers < 2 * tol)
     return
 
-def _mandatoryLevels(pres, tol=0.1):
+def _mandatoryPresLevels(pres, tol=0.1):
     '''
         Searches for the surface, 1000, 925, 850, 700,
         500, 400, 300, 250, 200, 150, 100, 70, 50, and 10 mb.
@@ -42,9 +42,50 @@ def _thermSigLevels(**snd):
     therm_sl = np.concatenate((inv_sl, iso_sl, add_sl))
     return np.sort(therm_sl)
 
+def reverseDiff(array):
+    return array[1:] + array[:-1]
+
+def _highestLevel(**snd):
+    return np.ma.argmin(snd['pres'])
+
+def _findTropopause(**snd):
+    '''
+        Finding the tropopause level using the WMO definition of a 
+        tropopause as being the lowest level where the 2 km layer
+        aloft has a lapse rate greater than 2 C/km.
+        
+        The algorithm below was taken from:
+        http://www.inscc.utah.edu/~reichler/publications/papers/2003_tropo.pdf
+    '''
+    above_500 = np.where((snd['pres'] > 75) & (snd['pres'] < 550))[0]
+    dT = np.diff(snd['temp'][above_500])
+    dp = np.diff(snd['pres'][above_500])
+    at = reverseDiff(snd['temp'][above_500])
+    ap = reverseDiff(snd['pres'][above_500])
+    ah = reverseDiff(snd['hght'][above_500])
+    R = 287.
+    g = 9.81
+    Cp = 1004.
+    kappa = R / Cp
+
+    halfp = ap / 2.
+    halfh = ah / 2.
+    halfgamma = (dT/dp) * (ap/at) * (kappa * g/R)
+    idx = np.where(halfgamma > 0.002)[0]
+    for i in idx:
+        hbot = halfh[i]
+        htop = hbot + 2000.
+        htop_idx = np.argmin(np.abs(halfh - htop))
+        mean_lapse = np.mean(halfgamma[i:htop_idx])
+        if mean_lapse > 0.002:
+            return np.ma.argmin(np.fabs(snd['pres']-halfp[i]))    
+    return np.nan
+
 def soundingFilter(**snd):
-    pres_ml = _mandatoryLevels(snd['pres'])
+    pres_ml = _mandatoryPresLevels(snd['pres'])
+    _findTropopause(**snd)
     therm_sl = _thermSigLevels(**snd)
+    
     wind_sl = _windSigLevels(**snd)
 
     pres_sl = np.union1d(therm_sl, wind_sl)
@@ -75,9 +116,11 @@ if __name__ == "__main__":
         'dp':'dewp',
         'pres':'pres',
         'wspd':'wspd',
-        'deg':'wdir'
+        'deg':'wdir',
+        'alt':'hght'
     }
 
     snd_file = nio.open_file('sgpnwsupa1sX11.b1.20130520.165157.cdf', mode='r')
-    snd = dict( (name_translate[k], snd_file.variables[k][:]) for k in ['tdry', 'dp', 'pres', 'wspd', 'deg'])
+    snd_file = nio.open_file('sgpnwsupa1sX11.b1.20130520.110839.cdf', mode='r')
+    snd = dict( (name_translate[k], snd_file.variables[k][:]) for k in ['tdry', 'dp', 'pres', 'wspd', 'deg', 'alt'])
     snd_filtered = soundingFilter(**snd)
