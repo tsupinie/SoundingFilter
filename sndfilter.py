b83b012c54eb7a11f116f598d06a9e2a775d7ba0
Greg Blumberg <wblumberg@ou.edu>
2015-01-16 11:59:24 -0600

    added mandatory pressure level function

--- sndfilter.py
+++ sndfilter.py
@@ -1,8 +1,6 @@
 
 import numpy as np
 
-import Nio as nio
-
 def _windSigLevels(**snd):
     idxs = np.arange(snd['wdir'].shape[0])
     np.random.shuffle(idxs)
@@ -16,6 +14,23 @@ def _findIsothermals(temp, pres, tol=0.5):
     cand_layers = np.where(lr_layers < 2 * tol)
     return
 
+def _mandatoryLevels(pres, tol=0.1):
+    '''
+        Searches for the surface, 1000, 925, 850, 700,
+        500, 400, 300, 250, 200, 150, 100, 70, 50, and 10 mb.
+    '''
+    mandatory_pres = np.asarray([1000, 925, 850, 700, 500, 400, 300, 250, 200,\
+                      150, 100, 70, 50, 10])
+    mandatory_pres_idx = np.ones(mandatory_pres.shape, dtype=int)
+    pres = np.round(pres,0)
+    for i in xrange(len(mandatory_pres_idx)):
+        ind = np.where(np.fabs(pres - mandatory_pres[i]) < tol)[0]
+        if len(ind) == 0:
+            ind = [-9999]
+        mandatory_pres_idx[i] = ind[0]
+
+    return mandatory_pres_idx
+
 def _findAddSigLevels(temp, pres):
     return
 
@@ -28,6 +43,7 @@ def _thermSigLevels(**snd):
     return np.sort(therm_sl)
 
 def soundingFilter(**snd):
+    pres_ml = _mandatoryLevels(snd['pres'])
     therm_sl = _thermSigLevels(**snd)
     wind_sl = _windSigLevels(**snd)
 
@@ -52,6 +68,8 @@ def soundingFilter(**snd):
     return snd_filtered
 
 if __name__ == "__main__":
+    import Nio as nio
+    from pylab import *
     name_translate = {
         'tdry':'temp',
         'dp':'dewp',
@@ -60,7 +78,6 @@ if __name__ == "__main__":
         'deg':'wdir'
     }
 
-    base_path = "/data6/tsupinie/20110524/arm/"
-    snd_file = nio.open_file("%s/sgpsondewnpnS05.b1.20110524.202900.cdf" % base_path, mode='r')
+    snd_file = nio.open_file('sgpnwsupa1sX11.b1.20130520.165157.cdf', mode='r')
     snd = dict( (name_translate[k], snd_file.variables[k][:]) for k in ['tdry', 'dp', 'pres', 'wspd', 'deg'])
     snd_filtered = soundingFilter(**snd)
